e742589953dd6b2ff72f24493a698acc29fb845a
shubham agrawal <subhek.agrawal@gmail.com>
2026-04-21 00:03:14 +0530

    code change

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD Demo 🚀');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
